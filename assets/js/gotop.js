86375c065252f9c4cba65c39c8221bf679422514
10944115 <92076402+10944115@users.noreply.github.com>
2021-12-11 13:39:21 +0800

    Add files via upload

---
+++ assets/js/gotop.js
@@ -0,0 +1,14 @@
+
+$('#gotop').click(function () {
+    $('html,body').animate({ scrollTop: 0 }); 
+    return false;
+});
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('#gotop').fadeIn();
+    } else {
+        $('#gotop').fadeOut();
+    }
+});
